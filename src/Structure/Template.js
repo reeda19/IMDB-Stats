b7a50a767e9b9a81e970856c39421b6adac72fd0
Alex Reed <alexreed100@gmail.com>
2021-02-16 22:58:25 -0500

    Initialize project using Create React App

---
+++ src/Structure/Template.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+export default function App() {
+    return (
+        <div className="wrapper">
+            <main style={{padding: '5rem 0', display: 'flex', justifyContent: 'center'}}>
+                <h1 style={{margin: '0', color: 'white', fontSize: '3.5rem'}} >CRA-Barebones-Template</h1>
+            </main>
+        </div>
+    )
+}
